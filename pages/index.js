d789230092c64c6c82bd6ad2c574976d098d4cce
RiverLemburg <lemburgr@gmail.com>
2025-09-14 19:32:51 -0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{padding:24,fontFamily:'system-ui'}}>
+      <h1>It works 🎉</h1>
+      <p>This is <code>pages/index.js</code>. If you see this, routing is fixed.</p>
+      <p><a href="/studio">Go to Template Studio</a></p>
+    </div>
+  );
+}
